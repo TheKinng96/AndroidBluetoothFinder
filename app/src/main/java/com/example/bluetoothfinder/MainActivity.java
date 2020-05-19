cc88ccb14fb08940ee219016a434de69fd960626
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-05-19 20:08:12 +0900

    added receivers

--- app/src/main/java/com/example/bluetoothfinder/MainActivity.java
+++ app/src/main/java/com/example/bluetoothfinder/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.bluetoothfinder;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +20,16 @@ public class MainActivity extends AppCompatActivity {
     Button button;
 
     BluetoothAdapter bluetoothAdapter;
-    
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+        }
+
+    };
+
     public void search (View view) {
         textView.setText("Searching...");
         button.setEnabled(false);
@@ -29,5 +43,16 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         textView = findViewById(R.id.textView);
         button = findViewById(R.id.button);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED);
+        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(broadcastReceiver, intentFilter);
+
+        bluetoothAdapter.startDiscovery();
     }
 }
